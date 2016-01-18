d71f54132f6b75db12d4b99e2f4c2734a0f207ed
lio <strali07@gmail.com>
2016-01-18 20:24:54 +0100

    improved straptoc.js

--- js/straptoc/straptoc.js
+++ js/straptoc/straptoc.js
@@ -1,23 +1,53 @@
 var maketoc = function(){
-	$("#toc > h2").css({
-    marginLeft:'12px'
-});
+	// $("#toc > h2").css({
+ //    marginLeft:'12px'
+	// });
+
+	$('#toc').append($('<a/>').append($('<span/>').text("[--]").addClass('li_h1')));
+	var ul0 = $("<ul/>");
+	$('#toc').append(ul0);
+
     for(var i = 0,	elems = $(":header"); i < elems.length; i++) {
     		var nameh1 = elems[i].innerHTML.trim();
-    		//alert(nameh1);
 	        elems[i].id = nameh1;
 	        var nameh1href = '#'+nameh1;
-	        if($('[id='+'"'+nameh1+'"'+']').prop("tagName") == 'H2'){
-	        	$('#toc').append($('<a/>')
-	        	.attr('href', nameh1href)
-	        	.css({'color': 'black', 'padding':'30px'})
-	        	.html(nameh1))
-	        	}
+	        if($('[id='+'"'+nameh1+'"'+']').prop("tagName") == 'H2')
+	        	{
+	        	var li = $("<li/>");
+	        	li.append(
+	        		$('<a/>')
+	        		.attr('href', nameh1href)
+	        		.html(nameh1)
+	        		.addClass(nameh1)
+	        		) // end of li
+	        		.css({'list-style': 'square inside','line-height': '20px'}) // end append li
+	        	ul.append(li)
+				} // end if h2
 	        else{
-	        $('#toc').append($('<a/>')
-	        	.attr('href', nameh1href)
-	        	.html(nameh1))
-	        }
-	        $('#toc').append($('<br/>')); // go to new line
+	        	var li0 = $("<li/>");
+				ul0.append(li0);
+	        	var ul = $("<ul/>").addClass(nameh1);
+	        	li0.append(
+		        	$('<a/>')
+		        	.attr('href', nameh1href)
+		        	.css({'color': 'black'})
+		        	.html(nameh1)
+		        	.append($('<span/>').text("[-]").addClass('li_h2')) // 
+       				) // en append ul
+       			li0.append(ul);
+	        } // end else
     } // end for elems
+  
+    $("a").click(function (evt) {   
+    	if(evt.target.className == 'li_h2') 
+            $(this).next().toggle();
+    	});
+
+    $("a").click(function (evt) {   
+    	if(evt.target.className == 'li_h1') 
+            //$(this).next().find('ul li').toggle();
+        	//alert('ohohoh');
+        	$(this).next().toggle();
+    	});
+
 }
