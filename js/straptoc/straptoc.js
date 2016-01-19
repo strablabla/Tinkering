042df49572a70ac8fff30bb99f2537692cf1e825
LC <lionel.chiron@gmail.com>
2016-01-19 17:56:16 +0100

    closing all submenus in straptoc

--- js/straptoc/straptoc.js
+++ js/straptoc/straptoc.js
@@ -1,53 +1,50 @@
 var maketoc = function(){
-	// $("#toc > h2").css({
- //    marginLeft:'12px'
-	// });
 
-	$('#toc').append($('<a/>').append($('<span/>').text("[--]").addClass('li_h1')));
-	var ul0 = $("<ul/>");
-	$('#toc').append(ul0);
+    $('#toc').append($('<a/>').append($('<span/>').text("[--]").addClass('li_h1')));
+    var ul0 = $("<ul/>");
+    $('#toc').append(ul0);
 
-    for(var i = 0,	elems = $(":header"); i < elems.length; i++) {
-    		var nameh1 = elems[i].innerHTML.trim();
-	        elems[i].id = nameh1;
-	        var nameh1href = '#'+nameh1;
-	        if($('[id='+'"'+nameh1+'"'+']').prop("tagName") == 'H2')
-	        	{
-	        	var li = $("<li/>");
-	        	li.append(
-	        		$('<a/>')
-	        		.attr('href', nameh1href)
-	        		.html(nameh1)
-	        		.addClass(nameh1)
-	        		) // end of li
-	        		.css({'list-style': 'square inside','line-height': '20px'}) // end append li
-	        	ul.append(li)
-				} // end if h2
-	        else{
-	        	var li0 = $("<li/>");
-				ul0.append(li0);
-	        	var ul = $("<ul/>").addClass(nameh1);
-	        	li0.append(
-		        	$('<a/>')
-		        	.attr('href', nameh1href)
-		        	.css({'color': 'black'})
-		        	.html(nameh1)
-		        	.append($('<span/>').text("[-]").addClass('li_h2')) // 
-       				) // en append ul
-       			li0.append(ul);
-	        } // end else
+    for(var i = 0,  elems = $(":header"); i < elems.length; i++) {
+            var nameh1 = elems[i].innerHTML.trim();
+            elems[i].id = nameh1;
+            var nameh1href = '#'+nameh1;
+            if($('[id=' + '"' + nameh1 + '"'+']').prop("tagName") == 'H2')
+                {
+                var li = $("<li/>");
+                li.append(
+                    $('<a/>')
+                    .attr('href', nameh1href)
+                    .html(nameh1)
+                    .addClass(nameh1)
+                    ) // end of li
+                    .css({'list-style': 'square inside','line-height': '20px'}) // end append li
+                ul.append(li)
+                } // end if h2
+            else{
+                var li0 = $("<li/>");
+                ul0.append(li0);
+                var ul = $("<ul/>").addClass('lev2');
+                li0.append(
+                    $('<a/>')
+                    .attr('href', nameh1href)
+                    .css({'color': 'black'})
+                    .html(nameh1)
+                    .append($('<span/>').text("[-]").addClass('li_h2')) // 
+                    ) // en append ul
+                li0.append(ul);
+            } // end else
     } // end for elems
+    
+    $('ul.lev2').toggle();
   
     $("a").click(function (evt) {   
-    	if(evt.target.className == 'li_h2') 
+        if(evt.target.className == 'li_h2') 
             $(this).next().toggle();
-    	});
+        });
 
     $("a").click(function (evt) {   
-    	if(evt.target.className == 'li_h1') 
-            //$(this).next().find('ul li').toggle();
-        	//alert('ohohoh');
-        	$(this).next().toggle();
-    	});
+        if(evt.target.className == 'li_h1') 
+            $(this).next().toggle();
+        });
 
 }
