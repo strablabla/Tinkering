1cbf874310276f5e4f0bd46afce7a48905d7af60
LC <lionel.chiron@gmail.com>
2016-01-22 10:37:13 +0100

    improved pythreejs

--- py/pyjson_threejs/simple_json_3d.py
+++ py/pyjson_threejs/simple_json_3d.py
@@ -6,26 +6,31 @@ class Three_json(object):
     '''
     Zoom 3D 
     '''
-    def __init__(self):
-        self.blabla = True
-        self.N = 500
-        self.ampl = 100
+    def __init__(self, ampl=None):
+        self.ampl = ampl
+        self.Nx = 300
+        self.Ny = 300
+    
+    def example(self):
+        self.ampl = 50
+        Z = np.empty((self.Nx, self.Ny))
+        for i in range(self.Nx):
+            for j in range(self.Ny):
+                Z[i,j] = (np.sin(i*np.pi/float(self.Nx)*4)+np.sin(j*np.pi/float(self.Ny)*4))
+        print Z.shape
+        return Z
 
-    def plot_shape_3d(self):
+    def plot_shape_3d(self, Z):
         '''
         Makes the 3D plot from the meshgrid.
         Makes the json that is read by FTICR2D_3d.html
         '''
         data = []
-        Z = np.empty((self.N, self.N))
-        for i in range(self.N):
-            for j in range(self.N):
-                Z[i,j] = self.ampl*(np.sin(i*np.pi/float(self.N)*4)+np.sin(j*np.pi/float(self.N)*4))
-        print Z.shape
+
         dimx, dimy = Z.shape[0], Z.shape[1]
         xflat = np.array([np.arange(dimx) for i in range(dimy)]).flatten()
         yflat = np.array([np.ones(dimx)*i for i in range(dimy)]).flatten()
-        zflat = Z.flatten()
+        zflat = self.ampl*Z.flatten()
         sizemat = xflat.size
         with io.open('3d.json', 'w', encoding ='utf-8') as f:
               for i in range(sizemat):
@@ -33,6 +38,7 @@ class Three_json(object):
                   data.append({'x':int(xflat[i]),'y':int(yflat[i]),'z': zflat[i] })
               f.write(unicode('data = '))
               f.write(unicode(json.dumps(data, ensure_ascii = False)))
-              
-tj = Three_json()
-tj.plot_shape_3d()
+
+if __name__=='__main__':
+    tj = Three_json()
+    tj.plot_shape_3d(tj.example())
