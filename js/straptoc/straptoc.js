d1ca30fe79db909062e3e81d162e97ff0435ac28
lio <strali07@gmail.com>
2016-01-23 01:09:49 +0100

    straptoc with 3 levels

--- js/straptoc/straptoc.js
+++ js/straptoc/straptoc.js
@@ -1,6 +1,6 @@
 var maketoc = function(){
     
-    var reg_free = /\d{1,2}\/\d{1,2}\/\d{2}/; //find dates whatever is its position.
+    var reg_free = /\d{1,2}\/\d{1,2}\/\d{2}/; //find dates whatever is its position with regexp
 
     $("p").each(function() {  // Replacing dates with p in date with h2 and 
         if ($(this).html().match(reg_free)){
@@ -13,50 +13,76 @@ var maketoc = function(){
     }); // end each
 
     $('#toc').append($('<a/>').append($('<span/>').text("[--]").addClass('li_h1')));
-    var ul0 = $("<ul/>"); // first levels with class
-    $('#toc').append(ul0);
+    var ul1 = $("<ul/>"); // first levels with class
+    $('#toc').append(ul1);
 
     for(var i = 0,  elems = $(":header"); i < elems.length; i++) {
             var nameh = elems[i].innerHTML.trim();
-            elems[i].id = nameh;
-            if($('[id=' + '"' + nameh + '"'+']').prop("tagName") == 'H2')
-                {
-                elems[i].id = nameh1 +'_'+ nameh;
-                var namehhref = '#'+elems[i].id;
-                var li = $("<li/>");
-                li.append(  // H1 tags
+            elems[i].id = nameh; 
+
+            if($('[id=' + '"' + nameh + '"'+']').prop("tagName") == 'H1')
+                {                        // if H1
+                var nameh1 = nameh
+                var li1 = $("<li/>");
+                ul1.append(li1);
+                var ul2 = $("<ul/>").addClass('lev1'); // second levels with class
+                li1.append(  
                     $('<a/>')
-                    .attr('href', namehhref)
+                    .attr('href', namehhref) // add ref to id done just before
+                    .css({'color': 'black'})
+                    .html(nameh)
+                    .append($('<span/>').text(" [-]").addClass('li_h2')) // button for H1
+                    ) // en append ul
+                li1.append(ul2);
+            } // end if H1
+
+            else if($('[id=' + '"' + nameh + '"'+']').prop("tagName") == 'H2')
+                {
+                elems[i].id = nameh1 +'_'+ nameh; // makes an id for H2
+                var nameh2 = elems[i].id
+                var namehhref = '#'+nameh2;
+                var li2 = $("<li/>");
+                ul2.append(li2);
+                var ul3 = $("<ul/>").addClass('lev2'); 
+                li2.append(  
+                    $('<a/>')
+                    .attr('href', namehhref) // adds ref to id done just before
+                    .html(nameh)
+                    .append($('<span/>').text(" [-]").addClass('li_h3')) // button for H2
+                    ) // end of li
+                    .css({'list-style': 'square inside','line-height': '20px'}) // end append li2
+                li2.append(ul3)
+                } // end else if H2
+
+            else if($('[id=' + '"' + nameh + '"'+']').prop("tagName") == 'H3')
+                {
+                elems[i].id = nameh2 +'_'+ nameh; // makes an id for H3
+                var nameh3 = elems[i].id
+                var namehhref = '#'+nameh3;
+                var li3 = $("<li/>");
+                ul3.append(li3);
+                //var ul4 = $("<ul/>").addClass('lev3'); 
+                li3.append(  
+                    $('<a/>')
+                    .attr('href', namehhref) // adds ref to id done just before
                     .html(nameh)
                     .addClass(nameh)
                     ) // end of li
-                    .css({'list-style': 'square inside','line-height': '20px'}) // end append li
-                ul.append(li)
-                } // end if h2
-            else{                        // if H1
-                var nameh1 = nameh
-                var li0 = $("<li/>");
-                ul0.append(li0);
-                var ul = $("<ul/>").addClass('lev2'); // second levels with class
-                li0.append(  // H2 tags
-                    $('<a/>')
-                    .attr('href', namehhref)
-                    .css({'color': 'black'})
-                    .html(nameh)
-                    .append($('<span/>').text("[-]").addClass('li_h2')) // 
-                    ) // en append ul
-                li0.append(ul);
-            } // end else
+                    .css({'list-style': 'circle inside','line-height': '20px'}) // end append li3
+                } // end else if H3
+
     } // end for elems
     
-    $('ul.lev2').toggle();                  // close all the  second levels
-    $("a").click(function (evt) {           // toggle for second levels
-        if(evt.target.className == 'li_h2') 
-            $(this).next().toggle();
-        });
-    $("a").click(function (evt) {           // toggle for first levels
+    $('ul.lev1').toggle();                  // 
+    $('ul.lev2').toggle();                  // 
+
+    $("a").click(function (evt) {           // toggle for H1
         if(evt.target.className == 'li_h1') 
             $(this).next().toggle();
+        else if(evt.target.className == 'li_h2') 
+            $(this).next().toggle();
+        else if(evt.target.className == 'li_h3') 
+            $(this).next().toggle();
         });
-
+ 
 }
