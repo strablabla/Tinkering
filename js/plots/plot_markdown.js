8df8e4f4e0e4c2badb4fc1badf17e767a97c1dde
lio <strali07@gmail.com>
2016-01-30 17:06:47 +0100

    improving plot

--- js/plots/plot_markdown.js
+++ js/plots/plot_markdown.js
@@ -1,23 +1,27 @@
-
-var plot = function(dom, data_curve, xoffset){
-
-    var pos_line = 150
+// Utility for making plots with d3.js
+var plot = function(dom, data_curve, xoffset, col){
+    var col = col || 'k';
+    var pos_line = 150;
     var width = 500,
         height = 200;
+    var curve_height = 100;
     var colrs = {'r':'red', 'k':'black', 'b':'blue', 'g':'green'};
-    var svg = d3.select(dom) //Create SVG element
+    var svg = d3.select(dom)                //Create SVG element
                 .append("svg")
                 .attr("width", width)
                 .attr("height", height);
-    var curve_height = 100;
-    var valueline = d3.svg.line() // Define the line 
-      .x(function(d) { return d.x*5; })
-      .y(function(d) { return d.y*curve_height + pos_line; }) // positionned on the line
+    var valueline = d3.svg.line()           // Define the line 
+        .x(function(d) { return d.x*5; })
+        .y(function(d) { return d.y*curve_height + pos_line; }) // positionned on the line
     curve = function(posx){
         var curved = svg.append("path")  
         .attr("class", "line")
         .attr("d", valueline(data_curve))
-        .attr("transform", "translate(" + posx.toString() + ", 0)" ) // Translationin x
+        .attr("transform", "translate(" + xoffset.toString() + ", 0)" ) // Translationin x
+        .style({stroke : colrs[col],
+                fill : 'none',
+                'stroke-width' : '1.5px',
+            })
         };
-    curve(xoffset)
-} // end graph
+    curve()
+} // end plot
