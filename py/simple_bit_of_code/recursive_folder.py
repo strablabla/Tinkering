f08413686d5ed8ca2eec2c2140e4b40d9a6dc8f7
lio <strali07@gmail.com>
2016-02-13 11:03:48 +0100

    improving recursive folder

--- py/simple_bit_of_code/recursive_folder.py
+++ py/simple_bit_of_code/recursive_folder.py
@@ -1,8 +1,15 @@
-def recurswalk(somepath): # Walking recursively in th folder
+import os,sys
+from os.path import join
+
+def recurswalk(somepath): # Walking recursively in the folder
     for root, dirs, files in os.walk(somepath):
         for curfile in files:
             yield join(root, curfile)
 
-for path in recurswalk(folder):
-    if path[-4:] == 'html':
-        print "path is ", path
+
+if __name__ == '__main__':
+	folder = sys.argv[1]
+	print folder
+	for path in recurswalk(folder):
+	    if path[-4:] == 'html' and path.find('_Notes')!=-1:
+	        print "path is ", path
