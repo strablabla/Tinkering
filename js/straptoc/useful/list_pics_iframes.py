76b21d530d92c55e9622f521ba5a2e1110560612
LC <lionel.chiron@gmail.com>
2016-05-12 11:45:20 +0200

    improving useful and css

--- js/straptoc/useful/list_pics_iframes.py
+++ js/straptoc/useful/list_pics_iframes.py
@@ -1,6 +1,6 @@
 # coding: utf8
 
-import os
+import sys, os
 import glob
 import re
 
@@ -9,26 +9,36 @@ Makes the list of pics or iframe for inserting the text in the doc.
 If the names are not with right syntax, it makes the corrections. 
 '''
 
-limages = ['.jpg', '.png']
-liframes = ['.html']
-dic_corr = {"'":"_", "é":"e","û":"u","û":"u",
-            "â":"a","ô":"o","à":"a","\(":"_","\)":"_",
-            "è":"e" }
+def list_pic_frames(corr):
 
-for i, name in enumerate(glob.glob('*.*')):
-    nameold = name
-    l = name.split()
-    for k in dic_corr: 
-        # print k
-        name = re.sub(k, dic_corr[k], name)
-        print name
-    if len(l) >1: 
-        name = '_'.join(l)
-    os.rename(nameold, name)
-    root, ext = os.path.splitext(name)
-    print ext
-    if ext in limages:
-        print '![%{0}%]({1})'.format(name[:-4], name)
-    elif ext in liframes:
-        print '[{0},,]({1})'.format(name[:-4], name)
-print i
+    limages = ['.jpg', '.png']
+    liframes = ['.html']
+    dic_corr = {"'":"_", "é":"e","û":"u","û":"u",
+                "â":"a","ô":"o","à":"a","\(":"_","\)":"_",
+                "è":"e" }
+
+    for i, name in enumerate(glob.glob('*.*')):
+        nameold = name
+        l = name.split()
+        for k in dic_corr: 
+            # print k
+            name = re.sub(k, dic_corr[k], name)
+            print name
+        if len(l) >1: 
+            name = '_'.join(l)
+        if corr:
+            os.rename(nameold, name)
+        root, ext = os.path.splitext(name)
+        print ext
+        if ext in limages:
+            print '![%{0}%]({1})'.format(name[:-4], name)
+        elif ext in liframes:
+            print '[{0},,]({1})'.format(name[:-4], name)
+    print i
+
+if __name__=='__main__':
+    try:
+        corr = sys.argv[1]
+    except: 
+        corr = False
+    list_pic_frames(corr) # if corr == True, make the corrections.
--- js/straptoc/useful/list_tree.py
+++ js/straptoc/useful/list_tree.py
@@ -1,20 +1,32 @@
-import os
 # coding: utf8
+import sys, os
+
 
 '''
-To place inside the folder
+All the addresses in the local folder with correction for inserting on infos_utiles.
 '''
 
-for path, dirs, files in os.walk('.'):
-	for f in files:
-		fold = f
-		l = f.split()
-		oldpath = os.path.join(path, fold)
-		if len(l) > 1: 
-			f = '_'.join(l)
-			newpath = os.path.join(path, f)
-			os.rename(oldpath , newpath)
-		else:
-			newpath = oldpath
-		root,ext = os.path.splitext(newpath)
-		print newpath[2:]
+def list_corr(corr):
+    for path, dirs, files in os.walk('.'):
+        for f in files:
+            fold = f
+            l = f.split()
+            oldpath = os.path.join(path, fold)
+            if len(l) > 1: 
+                f = '_'.join(l)
+                newpath = os.path.join(path, f)
+                if corr: 
+                    os.rename(oldpath , newpath)
+            else:
+                newpath = oldpath
+            root,ext = os.path.splitext(newpath)
+            print newpath[2:]
+        
+if __name__=='__main__':
+    try:
+        corr = sys.argv[1]
+    except: 
+        corr = False
+    list_corr(corr) # if corr == True, make the corrections.
+    
+        
