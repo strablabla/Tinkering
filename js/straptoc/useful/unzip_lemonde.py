53b000bc606182383115491b918d37a89d0afef5
lio <strali07@gmail.com>
2016-06-05 16:34:33 +0200

    improving lemonde pdf

--- js/straptoc/useful/unzip_lemonde.py
+++ js/straptoc/useful/unzip_lemonde.py
@@ -3,58 +3,83 @@ import sys, os, glob
 import zipfile
 
 '''
-All the addresses in the local folder with correction for inserting in infos_utiles.
+This program builds all  the addresses in the local folder with correction
+ for inserting the pdf in a straptoc document.
+syntax : lem corr
 '''
-def unzipfiles():
-    for f in glob.glob('*.zip'):
-        z = zipfile.ZipFile(f, 'r')
-        z.extractall('.')
-        z.close()
 
-def list_corr(corr):
-    prefix = raw_input("prefix? ")  # Prefix : folder containing all the files and folders.. 
-    dic_categ = {} # list categories
-    first = True
-    for path, dirs, files in os.walk('.'):
-        for f in files:
-            fold = f
-            l = f.split()
-            oldpath = os.path.join(path, fold)
-            if len(l) > 1: 
-                f = '_'.join(l) # remove empty spaces
-                newpath = os.path.join(path, f) # newpath
-                if corr: 
-                    os.rename(oldpath , newpath) # if correction asked (argument 'corr'), make correction
+class lemonde_straptoc(object):
+    def __init__(self):
+        self.prefix = raw_input("prefix? (folder containing all the files and folders) ") 
+        # self.pathfiles = os.getcwd()
+        # print self.pathfiles
+        #self.prefix = self.pathfiles
+        self.dic_categ = {} # list of the folders 
+        self.first = True
+
+    def unzipfiles(self): # extract all the zip files
+        for path, dirs, files in os.walk('.'):
+            # print "path {0}, dirs{1}, files{2} ".format(path, dirs, files)
+            # #print os.path.join(path, files)
+            pathzip = os.path.join(path,'*.zip')
+            print pathzip
+            for f in glob.glob(pathzip):
+                print f
+                z = zipfile.ZipFile(f, 'r')
+                z.extractall(path)
+                z.close()
+
+
+    def straptoc_pdf(self):
+        '''
+        Handles two levels of folders
+        '''
+        filename = os.path.basename(self.newpath[2:])[:-4]
+        nwps = self.newpath[2:].split('/')
+        if len(nwps) > 1: # subfolders
+            if nwps[0] not in self.dic_categ : 
+                self.dic_categ[nwps[0]] = 1 # initialize
+                category = '* {0} :: \n    * $pdf \n        +++ {1}/{0} \n'.format(nwps[0], self.prefix)  # Category
+                category += '    '
             else:
-                newpath = oldpath # no empty spaces
-            root,ext = os.path.splitext(newpath)
-            #print '############  ', newpath
-            if ext == '.pdf':
-                filename = os.path.basename(newpath[2:])[:-4]
-                nwps = newpath[2:].split('/')
-                if len(nwps)>1:
-                    
-                    if nwps[0] not in dic_categ : 
-                        dic_categ[nwps[0]] = 1 # initialize
-                        category = '* {0} :: \n    * $pdf \n     +++ {1}/{0} \n'.format(nwps[0], prefix)  # Category
-                    else:
-                        dic_categ[nwps[0]] += 1 # increment
-                        category = '    '
+                self.dic_categ[nwps[0]] += 1 # increment
+                category = '    '
+            # strpdf = category + '    [{0} §§]({0}.pdf)'.format(filename) #
+        else:  # no subfolder
+            if self.first:
+                category = '* $pdf \n    +++ {0} \n'.format(self.prefix)
+                self.first = False
+            else:
+                category = ''
+        strpdf = category + '    [{0} §§]({0}.pdf)'.format(filename) #
+        print strpdf
+
+    def list_corr(self, corr):
+
+        for path, dirs, files in os.walk('.'):
+            for f in files:
+                fold = f
+                l = f.split()
+                oldpath = os.path.join(path, fold)
+                if len(l) > 1: 
+                    f = '_'.join(l) # remove empty spaces
+                    self.newpath = os.path.join(path, f) # newpath
+                    if corr: 
+                        os.rename(oldpath , self.newpath) # if correction asked (argument 'corr'), make correction
                 else:
-                    if first:
-                        category = '* $pdf \n'
-                        first = False
-                    else:
-                        category = ''
-                strpdf = category + '    [{0} §§]({0}.pdf)'.format(filename) #
-                print strpdf
+                    self.newpath = oldpath # no empty spaces
+                root,ext = os.path.splitext(self.newpath)
+                #print '############  ', newpath
+                if ext == '.pdf':
+                    self.straptoc_pdf()
         
 if __name__=='__main__':
+    ls = lemonde_straptoc()
     try:
         corr = sys.argv[1]
     except: 
         corr = False
-    unzipfiles()
-    list_corr(corr) # if corr == True, make the corrections.
+    ls.unzipfiles()
+    ls.list_corr(corr) # if corr == True, make the corrections.
     
         
