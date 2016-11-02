653a5fdb3a19164c84b3c371c8a8e0a309bb6f68
lio_oneTwo <lio.chiron@gmail.com>
2016-11-02 10:49:19 +0100

    imprv unzip lem

--- js/straptoc/useful/unzip_lemonde.py
+++ js/straptoc/useful/unzip_lemonde.py
@@ -4,17 +4,17 @@ import zipfile
 
 '''
 Program for extracting the Lemonde pdf from zipfile and building
- the code to be inserted in the straptoc document. 
+ the code to be inserted in the straptoc document.
 syntax : lem corr
 '''
 
 class lemonde_straptoc(object):
     def __init__(self):
         try:
-            self.prefix = raw_input("prefix? (folder containing all the files and folders) ") 
+            self.prefix = raw_input("prefix? (folder containing all the files and folders) ")
         except:
             self.prefix = input("prefix? (folder containing all the files and folders) ")
-        self.dic_categ = {}                    # list of the folders 
+        self.dic_categ = {}                    # list of the folders
         self.first = True
         self.dict_pdfs = {}
 
@@ -50,26 +50,26 @@ class lemonde_straptoc(object):
         '''
         Producing the code to paste in the straptoc file.
         '''
-        dic_month = {1:'janv', 2: 'fev', 3:'mars', 4:'avr', 5: 'mai',
-              6:'juin', 7:'juill' , 8:'aout', 9:'sept',
-               10:'oct', 11:'nov', 12:'dec'}
+        dic_month = {1:'Janv', 2: 'Fev', 3:'Mars', 4:'Avr', 5: 'Mai',
+              6:'Juin', 7:'Juill' , 8:'Aout', 9:'Sept',
+               10:'Oct', 11:'Nov', 12:'Dec'}
         for y in [2014]:
             print('## '+ str(y))
-            
+
             for m in range(1,13):
                 try:
-                    test_exist = self.dict_pdfs[str(y)][str(m)]
-                    print('\n'+ '### '+ dic_month[m] +'\n')
-                    print('* $pdf \n    +++ {0} '.format(self.prefix))
+                    test_exist = self.dict_pdfs[str(y)][str(m)]  # test if the month exists
+                    print('* '+ dic_month[m] +'::')
+                    print('    * $pdf \n    +++ {0} '.format(self.prefix))
                 except:
                     pass
                 # print(self.dict_pdfs[str(y)])
-                for d in range(1,32):
+                for d in range(1,32): # Read each day
                     try:
                         list_pdfs = self.dict_pdfs[str(y)][str(m)][str(d)]
-                        # print('------------------'+ str(d) +'\n')
+                        print('    * ' + str(d) + '::')
                         for p in list_pdfs:
-                            print(p)
+                            print('        * ' + p)
                     except:
                         pass
 
@@ -79,7 +79,7 @@ class lemonde_straptoc(object):
     #     '''
     #     nwps = self.newpath[2:].split('/')           # Cut the address
     #     if len(nwps) > 1:                            # subfolders
-    #         if nwps[0] not in self.dic_categ : 
+    #         if nwps[0] not in self.dic_categ :
     #             self.dic_categ[nwps[0]] = 1          # initialize
     #             category = '* {0} :: \n    * $pdf \n        +++ {1}/{0} \n'.format(nwps[0], self.prefix)  # Category
     #             category += '    '
@@ -95,11 +95,11 @@ class lemonde_straptoc(object):
 
     def list_corr(self, arg):
         '''
-        Makes correction on the names. 
-        Launches once all the zips are dezipped. 
+        Makes correction on the names.
+        Launches once all the zips are dezipped.
         Parameters:
             * arg : if it equal to "corr", then make corrections
-                * remove empty space etc.. 
+                * remove empty space etc..
 
         '''
 
@@ -108,25 +108,23 @@ class lemonde_straptoc(object):
                 fold = f
                 l = f.split()
                 oldpath = os.path.join(path, fold)
-                if len(l) > 1: 
+                if len(l) > 1:
                     f = '_'.join(l)                        # remove empty spaces
                     self.newpath = os.path.join(path, f)   # newpath
-                    if arg == 'corr': 
+                    if arg == 'corr':
                         os.rename(oldpath , self.newpath)  # if correction asked (argument 'corr'), make correction
                 else:
                     self.newpath = oldpath                 # no empty spaces
                 root,ext = os.path.splitext(self.newpath)
                 if ext == '.pdf':
                     self.prepare_straptoc_pdf()                    # Code for straptoc
-        
+
 if __name__=='__main__':
     ls = lemonde_straptoc()
     try:
         arg = sys.argv[1]
-    except: 
+    except:
         arg = False
     ls.unzipfiles()      # unzip all the files
-    ls.list_corr(arg)    # if corr 
+    ls.list_corr(arg)    # if corr
     ls.show_straptoc_pdf()
-    
-        
