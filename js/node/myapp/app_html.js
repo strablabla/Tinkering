0e211af41e850b4160a4d34a9ba6afcbda68f98f
liohp <lio.chiron@gmail.com>
2016-11-05 23:25:23 +0100

    improving node

--- js/node/myapp/app_html.js
+++ js/node/myapp/app_html.js
@@ -1,11 +1,57 @@
 var express = require('express')
-var app = express()
+var path = require("path");
+var fs = require('fs');
 
-var path    = require("path");
+var app = express()
+var server = require('http').createServer(app);
 
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/views/strap_small.html'));
-  //__dirname : It will resolve to your project folder.
+      res.sendFile(path.join(__dirname+'/views/strap_small.html'));
+  });
+
+app.get('/text',function(req,res){
+      res.sendFile(path.join(__dirname+'/views/text.html'));
+  });
+
+var w = false
+var r = false
+
+if (w == true){
+
+    fs.writeFile("bingo.txt", "Hey there!", function(err) {
+	    if(err) {
+	        return console.log(err);
+	    }
+	    console.log("The file was saved!");
+    }); 
+}
+
+if (r == true){
+
+	//fs.readFile('bingo.txt', 'utf8', function (err,data) {
+	fs.readFile('views/strap_small.html', 'utf8', function (err,data) {
+		  if (err) {
+		    return console.log(err);
+		  }
+		  console.log(data);
+	});
+}
+
+// Loading socket.io
+
+var io = require('socket.io')(server);
+
+// When a client connects, we note it in the console
+
+io.sockets.on('connection', function (socket) {
+    console.log('A client is connected!');
+    io.sockets.emit('message', 'You are connected!');
+
 });
 
-app.listen(3000);
+io.sockets.on('join', function(data) {
+    console.log(data);
+  });
+
+
+server.listen(3000);
