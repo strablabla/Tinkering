fe319f9e81f06008cd648a7498a151df0c13d28b
lio_oneTwo <lio.chiron@gmail.com>
2016-11-06 22:07:41 +0100

    beginning for retriving positionin node strap text

--- js/node/myapp/html_app.js
+++ js/node/myapp/html_app.js
@@ -38,14 +38,46 @@ if (r == true){
 // Loading socket.io
 var io = require('socket.io')(server);
 // When a client connects, we note it in the consol
+// var transm_ratio = 0
 io.sockets.on('connection', function (socket) {
     console.log('A client is connected!');
     // io.sockets.emit('message', 'You are connected!');
+    // io.sockets.emit('scroll', transm_ratio);
     fs.readFile('views/strap_small.html', 'utf8', function (err,data) {
         if (err) {
           return console.log(err);
         }
         io.sockets.emit('message', data); // send the text to the client side.
+
+        var save_current_version = function(data){
+          var date = new Date()
+          var minute = date.getMinutes();
+          var hour = date.getHours();
+          var day = date.getDate();
+          var month = date.getMonth();
+          var year = date.getFullYear();
+          var txt_date = [year, month, day, hour, minute].join('_')
+          fs.writeFile("views/saved/strap_small" + '_old_' + txt_date + ".html", data, function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+          }); // end writeFile
+        } // end save_current_version
+
+        fs.readFile('views/scroll.json', 'utf8', function (err, ratio) {
+            if (err) {
+              return console.log(err);
+            }
+            console.log("ratio from readFile is : " + ratio);
+            io.sockets.emit('scroll', ratio); // send the text to the client side.
+        });
+
+        setInterval(function(data) { // Save the current strap version.
+                // Do something every 1 minute (60000 s)
+                save_current_version(data)
+          }, 60000);
+
     }); // end fs.readFile
 
     socket.on('join', function(data) {
@@ -55,13 +87,24 @@ io.sockets.on('connection', function (socket) {
 
     socket.on('return', function(data) {
         console.log('Retrieving the whole text modified... ' + data);
-        fs.writeFile("views/strap_small_diff.html", data, function(err) {
+        fs.writeFile("views/strap_small.html", data, function(err) {
           if(err) {
               return console.log(err);
           }
-          console.log("The file was saved!");
+          console.log("The file was modified!");
         });
       }); // end socket.on return
+
+
+      socket.on('scroll', function(ratio) {
+          console.log('ratio is ... ' + ratio);
+          fs.writeFile("views/scroll.json", ratio, function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("saving scroll in json");
+          }); // end writeFile
+        }); // end socket.on scroll
 
 }); // sockets.on connection
 
