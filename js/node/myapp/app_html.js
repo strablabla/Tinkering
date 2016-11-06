10990bb921586d2f04ca5106eedfa45e8e61e27d
lio_oneTwo <lio.chiron@gmail.com>
2016-11-06 12:27:21 +0100

    node textarea for straptoc

--- js/node/myapp/app_html.js
+++ js/node/myapp/app_html.js
@@ -17,17 +17,15 @@ var w = false
 var r = false
 
 if (w == true){
-
     fs.writeFile("bingo.txt", "Hey there!", function(err) {
 	    if(err) {
 	        return console.log(err);
 	    }
 	    console.log("The file was saved!");
-    }); 
+    });
 }
 
 if (r == true){
-
 	//fs.readFile('bingo.txt', 'utf8', function (err,data) {
 	fs.readFile('views/strap_small.html', 'utf8', function (err,data) {
 		  if (err) {
@@ -38,20 +36,21 @@ if (r == true){
 }
 
 // Loading socket.io
-
 var io = require('socket.io')(server);
-
-// When a client connects, we note it in the console
-
+// When a client connects, we note it in the consol
 io.sockets.on('connection', function (socket) {
     console.log('A client is connected!');
-    io.sockets.emit('message', 'You are connected!');
+    // io.sockets.emit('message', 'You are connected!');
+    fs.readFile('views/strap_small.html', 'utf8', function (err,data) {
+        if (err) {
+          return console.log(err);
+        }
+        io.sockets.emit('message', data); // send the text to the client side.
+    });
+}); // sockets.on connection
 
-});
-
-io.sockets.on('join', function(data) {
-    console.log(data);
-  });
-
+io.sockets.on('join', function() {
+    console.log('client sent a message... ');
+  }); // end socket.on join
 
 server.listen(3000);
