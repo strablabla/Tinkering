26b6d3b421276f84069530c78927cb9360c4d952
lio_oneTwo <lio.chiron@gmail.com>
2017-05-01 20:28:02 +0200

    improving chess

--- js/threejs_exp/drag/basic_chess/make_chess.js
+++ js/threejs_exp/drag/basic_chess/make_chess.js
@@ -1,25 +1,46 @@
 function make_board(){
     //alert("in make_board")
-    var size = 150;
-    var geometry = new THREE.CubeGeometry( size, size, 5 );
+    var size_square = 150;
+    var geometry = new THREE.CubeGeometry( size_square, size_square, 5 );
     var square_color
     for ( var i = 0; i < 64; i ++ ) {
-
         if ((i+Math.floor(i/8))%2==0){square_color = 0x000000}
         else{square_color = 0xffffff}
         var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: square_color } ) );
-
         object.material.ambient = object.material.color;
-
-        object.position.x = (i%8-4) * size
-        object.position.y = (Math.floor(i/8)-4) * size
+        object.position.x = (i%8-4) * size_square
+        object.position.y = (Math.floor(i/8)-4) * size_square
         object.position.z = 0
         object.castShadow = true;
         object.receiveShadow = true;
-
         scene.add( object );
         // objects.push( object );
-
     }
+} // end function
 
+function make_pawns(){
+    //alert("in make_pawns")
+    var size_square = 150;
+    var size_pawns = 50;
+    var geometry = new THREE.CubeGeometry( size_pawns, size_pawns, size_pawns );
+    var square_color;
+    for ( var i = 0; i < 16; i ++ ) {
+        if (Math.floor(i/8)%2==0){
+            square_color = 0x800000
+            var posy = 1
+           }
+        else{
+            square_color = 0xf0f0f5
+            var posy = 6
+           }
+        var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: square_color } ) );
+        object.material.ambient = object.material.color;
+        object.position.x = (i%8-4) * size_square
+        object.position.y = (posy-4) * size_square
+        object.position.z = 5
+        object.castShadow = true;
+        object.receiveShadow = true;
+        scene.add( object );
+        objects.push( object );
+    } // end for
 } // end function
