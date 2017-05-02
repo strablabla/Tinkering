d15414b35874b480365675b78fe27a0ec7f14d75
chiron <lionel.chiron@gmail.com>
2017-05-02 08:03:00 +0200

    adding towers..not finished

--- js/threejs_exp/drag/basic_chess/make_chess.js
+++ js/threejs_exp/drag/basic_chess/make_chess.js
@@ -44,3 +44,31 @@ function make_pawns(){
         objects.push( object );
     } // end for
 } // end function
+
+function make_towers(){
+    //alert("in make_pawns")
+    var size_square = 150;
+    var size_towers = 50;
+    var geometry = new THREE.CylinderGeometry(  50, 25, 100);
+    var square_color;
+    for ( var i = 0; i < 4; i ++ ) {
+        if (Math.floor(i/2)%2==0){
+            square_color = 0x800000
+            var posy = 1
+           }
+        else{
+            square_color = 0xf0f0f5
+            var posy = 6
+           }
+        var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: square_color } ) );
+        object.material.ambient = object.material.color;
+        object.position.x = (i%8-4) * size_square
+        object.position.y = (posy-4) * size_square
+        object.position.z = 100
+        object.rotation.x = -Math.PI/2
+        object.castShadow = true;
+        object.receiveShadow = true;
+        scene.add( object );
+        objects.push( object );
+    } // end for
+} // end function
