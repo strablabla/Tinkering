be35fe6c65d2b359f84ea92ae83dbeb3ebf2739c
chiron <lionel.chiron@gmail.com>
2017-05-05 14:09:49 +0200

    beginning 3D joystick

---
+++ js/threejs_exp/virtual_joystick/basic_chess/make_chess.js
@@ -0,0 +1,74 @@
+function make_board(){
+    //alert("in make_board")
+    var size_square = 150;
+    var geometry = new THREE.CubeGeometry( size_square, size_square, 5 );
+    var square_color
+    for ( var i = 0; i < 64; i ++ ) {
+        if ((i+Math.floor(i/8))%2==0){square_color = 0x000000}
+        else{square_color = 0xffffff}
+        var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: square_color } ) );
+        object.material.ambient = object.material.color;
+        object.position.x = (i%8-4) * size_square
+        object.position.y = (Math.floor(i/8)-4) * size_square
+        object.position.z = 0
+        object.castShadow = true;
+        object.receiveShadow = true;
+        scene.add( object );
+        // objects.push( object );
+    }
+} // end function
+
+function make_pawns(){
+    //alert("in make_pawns")
+    var size_square = 150;
+    var size_pawns = 50;
+    var geometry = new THREE.CubeGeometry( size_pawns, size_pawns, size_pawns );
+    var square_color;
+    for ( var i = 0; i < 16; i ++ ) {
+        if (Math.floor(i/8)%2==0){
+            square_color = 0x800000
+            var posy = 1
+           }
+        else{
+            square_color = 0xf0f0f5
+            var posy = 6
+           }
+        var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: square_color } ) );
+        object.material.ambient = object.material.color;
+        object.position.x = (i%8-4) * size_square
+        object.position.y = (posy-4) * size_square
+        object.position.z = 5
+        object.castShadow = true;
+        object.receiveShadow = true;
+        scene.add( object );
+        objects.push( object );
+    } // end for
+} // end function
+
+function make_towers(){
+    //alert("in make_pawns")
+    var size_square = 150;
+    var size_towers = 50;
+    var geometry = new THREE.CylinderGeometry(  50, 25, 100);
+    var square_color;
+    for ( var i = 0; i < 4; i ++ ) {
+        if (Math.floor(i/2)%2==0){
+            square_color = 0x800000
+            var posy = 1
+           }
+        else{
+            square_color = 0xf0f0f5
+            var posy = 6
+           }
+        var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: square_color } ) );
+        object.material.ambient = object.material.color;
+        object.position.x = (i%8-4) * size_square
+        object.position.y = (posy-4) * size_square
+        object.position.z = 100
+        object.rotation.x = -Math.PI/2
+        object.castShadow = true;
+        object.receiveShadow = true;
+        scene.add( object );
+        objects.push( object );
+    } // end for
+} // end function
