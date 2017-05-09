306130f8c9530401e09580e80bb2ef42e1cf7ba2
chiron <lionel.chiron@gmail.com>
2017-05-09 16:57:45 +0200

    presa web

---
+++ js/threejs_exp/presa_web/drag_flat.js
@@ -0,0 +1,93 @@
+function init_drag(){
+
+    projector = new THREE.Projector();
+    plane = new THREE.Mesh( new THREE.PlaneGeometry( 2000, 2000, 8, 8 ), new THREE.MeshBasicMaterial( { color: 0x000000, opacity: 0.25, transparent: true, wireframe: true } ) );
+    plane.visible = true;
+    scene.add( plane );
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onDocumentMouseMove( event ) {
+
+    event.preventDefault();
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+    var vector = new THREE.Vector3( mouse.x, mouse.y, 0.5 );
+    projector.unprojectVector( vector, camera );
+    var raycaster = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+
+    if ( SELECTED ) {
+
+        var intersects = raycaster.intersectObject( plane );
+        SELECTED.position.copy( intersects[ 0 ].point.sub( offset ) );
+        return;
+
+    }
+
+    var intersects = raycaster.intersectObjects( objects );
+    if ( intersects.length > 0 ) {
+
+        if ( INTERSECTED != intersects[ 0 ].object ) {
+
+            // 0xf0f0f5
+            // INTERSECTED.currentHex
+            if ( INTERSECTED ) INTERSECTED.material.color.setHex( INTERSECTED.currentHex); //
+            INTERSECTED = intersects[ 0 ].object;
+            INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+
+        }
+
+        container.style.cursor = 'pointer';
+
+    } else {
+        // 0xf0f0f5
+        // INTERSECTED.currentHex
+        if ( INTERSECTED ) INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+
+        INTERSECTED = null;
+        container.style.cursor = 'auto';
+
+    }
+
+}
+
+function onDocumentMouseDown( event ) {
+
+    event.preventDefault();
+    var vector = new THREE.Vector3( mouse.x, mouse.y, 0.5 );
+    projector.unprojectVector( vector, camera );
+    var raycaster = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+    var intersects = raycaster.intersectObjects( objects );
+
+    if ( intersects.length > 0 ) {
+
+        controls.enabled = false;
+        SELECTED = intersects[ 0 ].object;
+        var intersects = raycaster.intersectObject( plane );
+        container.style.cursor = 'move';
+
+    }
+
+    if ( INTERSECTED ) INTERSECTED.material.color.setHex( 0x000000 ); // changing color when selected
+
+}
+
+function onDocumentMouseUp( event ) {
+
+    event.preventDefault();
+    controls.enabled = true;
+    if ( INTERSECTED ) {
+        SELECTED = null;
+    }
+    container.style.cursor = 'auto';
+
+}
