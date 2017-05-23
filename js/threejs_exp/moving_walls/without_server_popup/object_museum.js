56cd2fc539c5143a63d98703179387ae1c58d21d
chiron <lionel.chiron@gmail.com>
2017-05-23 14:16:27 +0200

    improving chess

--- js/threejs_exp/moving_walls/without_server_popup/object_museum.js
+++ js/threejs_exp/moving_walls/without_server_popup/object_museum.js
@@ -1,4 +1,19 @@
-function make_board(){
+disp = 5
+function make_uniform_ground(){
+    //alert("in make_board")
+    var size_square = 1100;
+    var geometry = new THREE.CubeGeometry( size_square, size_square, 5 );
+    var square_color = 0xffffff
+    var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: square_color } ) );
+    object.material.ambient = object.material.color;
+    object.position.x = 0
+    object.position.y = 0
+    object.position.z = 0
+    object.castShadow = true;
+    object.receiveShadow = true;
+    scene.add( object );
+} // end function
+function make_ground_chess(){
     //alert("in make_board")
     var size_square = 150;
     var geometry = new THREE.CubeGeometry( size_square, size_square, 5 );
@@ -18,8 +33,28 @@ function make_board(){
     }
 } // end function
 
-function make_pawns(){
-    //alert("in make_pawns")
+function make_wall(){
+    //alert("in make_wall")
+    wall_color = 0x800000
+    var wall_length = 150;
+    var wall_width = 5;
+    var wall_height = 300;
+    var geometry = new THREE.CubeGeometry( wall_width, wall_length, wall_height );
+    var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: wall_color } ) );
+    object.material.ambient = object.material.color;
+    object.position.x = 0
+    object.position.y = 0
+    object.position.z = wall_height/2
+    object.castShadow = true;
+    object.receiveShadow = true;
+    scene.add( object );
+    objects.push( object );
+    return object
+
+} // end function
+
+function make_small_seats(){
+    //alert("in make_small_seats")
     var size_square = 150;
     var size_pawns = 50;
     var geometry = new THREE.CubeGeometry( size_pawns, size_pawns, size_pawns );
@@ -35,8 +70,8 @@ function make_pawns(){
            }
         var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: square_color } ) );
         object.material.ambient = object.material.color;
-        object.position.x = (i%8-4) * size_square
-        object.position.y = (posy-4) * size_square
+        object.position.x = disp*Math.random() * size_square
+        object.position.y = disp*Math.random() * size_square
         object.position.z = size_pawns/2
         object.castShadow = true;
         object.receiveShadow = true;
@@ -45,27 +80,26 @@ function make_pawns(){
     } // end for
 } // end function
 
-function make_towers(){
+function make_seat(){
+    //alert("in make_pawns")
     var size_square = 150;
-    var size_towers = 100;
-    var geometry = new THREE.CylinderGeometry(  50, 25, size_towers);
+    var size_seat = 100;
+    var geometry = new THREE.CylinderGeometry( 50, 25, size_seat);
     var square_color;
-    //pos_towers = [-650]
     for ( var i = 0; i < 4; i ++ ) {
         if (Math.floor(i/2)%2==0){
             square_color = 0x800000
-            var posy = 0
+            var posy = 1
            }
         else{
             square_color = 0xf0f0f5
-            var posy = 7
+            var posy = 6
            }
         var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: square_color } ) );
         object.material.ambient = object.material.color;
-        object.position.x = (Math.pow(-1, i%2)*3.5-0.5)* size_square
-        alert(object.position.x)
-        object.position.y = (posy-4) * size_square
-        object.position.z = size_towers/2
+        object.position.x = disp*Math.random() * size_square
+        object.position.y = disp*Math.random() * size_square
+        object.position.z = size_seat/2
         object.rotation.x = -Math.PI/2
         object.castShadow = true;
         object.receiveShadow = true;
@@ -73,6 +107,3 @@ function make_towers(){
         objects.push( object );
     } // end for
 } // end function
-
-
-
