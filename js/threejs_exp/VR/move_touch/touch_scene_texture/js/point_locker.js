7e50e9b5c09c05107a96f60d15f5af7460bb47d0
lio_oneTwo <lio.chiron@gmail.com>
2017-07-08 17:21:22 +0200

    touch gallery

--- js/threejs_exp/VR/move_touch/touch_scene_texture/js/point_locker.js
+++ js/threejs_exp/VR/move_touch/touch_scene_texture/js/point_locker.js
@@ -63,9 +63,9 @@ var posy = 150
 
 function init() {
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
     scene = new THREE.Scene();
-    scene.fog = new THREE.Fog( 0xffffff, 0, 750 );
+    scene.fog = new THREE.Fog( 0xffffff, 0, 20000 );
     var light = new THREE.HemisphereLight( 0xeeeeff, 0x777788, 0.75 );
     light.position.set( 0.5, 1, 0.75 );
     scene.add( light );
