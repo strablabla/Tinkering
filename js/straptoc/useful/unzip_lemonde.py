79527467319d5b094deb39d666a039efe09b47a0
lio_oneTwo <lio.chiron@gmail.com>
2017-10-28 15:51:08 +0200

    improving lemonde

--- js/straptoc/useful/unzip_lemonde.py
+++ js/straptoc/useful/unzip_lemonde.py
@@ -1,6 +1,10 @@
 # coding: utf8
 import sys, os, glob
+import shutil as sh
 import zipfile
+opj = os.path.join
+opd= os.path.dirname
+opb = os.path.basename
 
 '''
 Program for extracting the Lemonde pdf from zipfile and building
@@ -17,15 +21,34 @@ class lemonde_straptoc(object):
         self.dic_categ = {}                    # list of the folders
         self.first = True
         self.dict_pdfs = {}
+        self.dic_month = {1:'Janv', 2: 'Fev', 3:'Mars', 4:'Avr', 5: 'Mai',
+                      6:'Juin', 7:'Juill' , 8:'Aout', 9:'Sept',
+                       10:'Oct', 11:'Nov', 12:'Dec'}
 
     def unzipfiles(self):                      # extract all the zip files
         for path, dirs, files in os.walk('.'):
             pathzip = os.path.join(path,'*.zip')
             print(pathzip)
-            for f in glob.glob(pathzip):
-                print(f)
-                z = zipfile.ZipFile(f, 'r')
-                z.extractall(path)
+
+            for fzip in glob.glob(pathzip):
+                newdir = fzip[:-4]
+                print(newdir)
+                dirname = opd(newdir)
+                basename = opb(newdir)
+                numbmonth = int(basename.split('_')[2][4:6])
+                year = basename.split('_')[2][:4]
+                dirmonth = self.dic_month[numbmonth]
+                print(numbmonth)
+                print(dirmonth)
+                if not os.path.exists(year):
+                    os.mkdir(year)
+                yearmonth = opj(year,dirmonth)
+                if not os.path.exists(yearmonth):
+                    os.mkdir(yearmonth)
+                fullpath = opj(dirname,yearmonth,basename)
+                print(fzip)
+                z = zipfile.ZipFile(fzip, 'r')
+                z.extractall(fullpath)
                 z.close()
 
     def prepare_straptoc_pdf(self):
@@ -36,30 +59,29 @@ class lemonde_straptoc(object):
         year = filename[:4]
         month = filename[4:6]
         day = filename[6:8]
+        fullpath = opj(year, self.dic_month[int(month)], 'Le_Monde_'+filename[:8], filename)
         if year not in self.dict_pdfs:
             self.dict_pdfs[year] = {}
         if month not in self.dict_pdfs[year]:
             self.dict_pdfs[year][month] = {}
         if day not in self.dict_pdfs[year][month]:
             self.dict_pdfs[year][month][day] = []
-        kind = '_'+filename[-3:]
-        strpdf = '    [{0} §§]({1}.pdf)'.format(day + kind, filename) # straptoc syntax for pdf files
+        kind = '_' + filename[-3:]
+        strpdf = '    [{0} §§]({1}.pdf)'.format(day + kind, fullpath) # straptoc syntax for pdf files
         self.dict_pdfs[year][month][day].append(strpdf)
 
     def show_straptoc_pdf(self):
         '''
         Producing the code to paste in the straptoc file.
         '''
-        dic_month = {1:'Janv', 2: 'Fev', 3:'Mars', 4:'Avr', 5: 'Mai',
-              6:'Juin', 7:'Juill' , 8:'Aout', 9:'Sept',
-               10:'Oct', 11:'Nov', 12:'Dec'}
+
         for y in [2014]:
             print('## '+ str(y))
 
             for m in range(1,13):
                 try:
                     test_exist = self.dict_pdfs[str(y)][str(m)]  # test if the month exists
-                    print('* '+ dic_month[m] +'::')
+                    print('* ' + self.dic_month[m] + '::')
                     print('    * $pdf \n    +++ {0} '.format(self.prefix))
                 except:
                     pass
