626b19dae04e42f45881f60fceb4dac39b142ddf
lio_oneTwo <lio.chiron@gmail.com>
2017-10-28 17:00:41 +0200

    improving lemonde again

--- js/straptoc/useful/unzip_lemonde.py
+++ js/straptoc/useful/unzip_lemonde.py
@@ -2,9 +2,11 @@
 import sys, os, glob
 import shutil as sh
 import zipfile
+from collections import defaultdict
 opj = os.path.join
 opd= os.path.dirname
 opb = os.path.basename
+
 
 '''
 Program for extracting the Lemonde pdf from zipfile and building
@@ -24,6 +26,7 @@ class lemonde_straptoc(object):
         self.dic_month = {1:'Janv', 2: 'Fev', 3:'Mars', 4:'Avr', 5: 'Mai',
                       6:'Juin', 7:'Juill' , 8:'Aout', 9:'Sept',
                        10:'Oct', 11:'Nov', 12:'Dec'}
+        self.dict_pdfs = defaultdict(lambda: defaultdict(dict))
 
     def unzipfiles(self):                      # extract all the zip files
         for path, dirs, files in os.walk('.'):
@@ -35,40 +38,43 @@ class lemonde_straptoc(object):
                 print(newdir)
                 dirname = opd(newdir)
                 basename = opb(newdir)
-                numbmonth = int(basename.split('_')[2][4:6])
-                year = basename.split('_')[2][:4]
-                dirmonth = self.dic_month[numbmonth]
-                print(numbmonth)
-                print(dirmonth)
-                if not os.path.exists(year):
-                    os.mkdir(year)
-                yearmonth = opj(year,dirmonth)
-                if not os.path.exists(yearmonth):
-                    os.mkdir(yearmonth)
-                fullpath = opj(dirname,yearmonth,basename)
+                namedate = basename.split('_')[2]
+                y, m, d = self.extract_ymd(namedate)
+                if not os.path.exists(y):
+                    os.mkdir(y)
+                ym = opj(y,m)
+                if not os.path.exists(ym):
+                    os.mkdir(ym)
+                fullpath = opj(dirname,ym,basename)
                 print(fzip)
                 z = zipfile.ZipFile(fzip, 'r')
                 z.extractall(fullpath)
                 z.close()
+    def extract_ymd(self, name):
+        '''
+        Extract year, month, day
+        '''
+        y = name[:4]
+        m = self.dic_month[int(name[4:6])]
+        d = name[6:8]
+        return y, m, d
 
     def prepare_straptoc_pdf(self):
         '''
         Prepare the dic for inserting the pdf into straptoc files.
         '''
         filename = os.path.basename(self.newpath[2:])[:-4]
-        year = filename[:4]
-        month = filename[4:6]
-        day = filename[6:8]
-        fullpath = opj(year, self.dic_month[int(month)], 'Le_Monde_'+filename[:8], filename)
-        if year not in self.dict_pdfs:
-            self.dict_pdfs[year] = {}
-        if month not in self.dict_pdfs[year]:
-            self.dict_pdfs[year][month] = {}
-        if day not in self.dict_pdfs[year][month]:
-            self.dict_pdfs[year][month][day] = []
-        kind = '_' + filename[-3:]
-        strpdf = '    [{0} §§]({1}.pdf)'.format(day + kind, fullpath) # straptoc syntax for pdf files
-        self.dict_pdfs[year][month][day].append(strpdf)
+        y, m, d = self.extract_ymd(filename)
+        print(y, m, d)
+        fullpath = opj(y, m, 'Le_Monde_' + filename[:8], filename)
+        try:
+            if len(self.dict_pdfs[y][m][d]) >= 0:
+                kind = '_' + filename[-3:]
+                strpdf = '    [{0} §§]({1}.pdf)'.format(d + kind, fullpath) # straptoc syntax for pdf files
+                self.dict_pdfs[y][m][d].append(strpdf)
+                print(self.dict_pdfs)
+        except:
+            self.dict_pdfs[y][m][d] = []
 
     def show_straptoc_pdf(self):
         '''
@@ -78,42 +84,34 @@ class lemonde_straptoc(object):
         for y in [2014]:
             print('## '+ str(y))
 
-            for m in range(1,13):
+            for mth in range(1,13):
+                m = self.dic_month[mth]
+                m_str = ''
                 try:
-                    test_exist = self.dict_pdfs[str(y)][str(m)]  # test if the month exists
-                    print('* ' + self.dic_month[m] + '::')
-                    print('    * $pdf \n    +++ {0} '.format(self.prefix))
+                    test_exist = self.dict_pdfs[str(y)][m]  # test if the month exists
+                    m_str += '* ' + m + '::\n'
+                    m_str += '    * $pdf \n    +++ {0} \n'.format(self.prefix)
                 except:
                     pass
                 # print(self.dict_pdfs[str(y)])
+
+                dtot = ''
                 for d in range(1,32): # Read each day
+                    d_str = ''
+                    dpdfs_str = ''
                     try:
-                        list_pdfs = self.dict_pdfs[str(y)][str(m)][str(d)]
-                        print('    * ' + str(d) + '::')
+                        list_pdfs = self.dict_pdfs[str(y)][m][str(d)]
+                        d_str += '    * ' + str(d) + '::\n'
                         for p in list_pdfs:
-                            print('        * ' + p)
+                            dpdfs_str += '        * ' + p + '\n'
+                        if dpdfs_str != '':
+                            dtot += d_str + dpdfs_str
                     except:
                         pass
+                if dtot != '':
+                    print(m_str)
+                    print(dtot)
 
-    # def make_straptoc_pdf(self):
-    #     '''
-
-    #     '''
-    #     nwps = self.newpath[2:].split('/')           # Cut the address
-    #     if len(nwps) > 1:                            # subfolders
-    #         if nwps[0] not in self.dic_categ :
-    #             self.dic_categ[nwps[0]] = 1          # initialize
-    #             category = '* {0} :: \n    * $pdf \n        +++ {1}/{0} \n'.format(nwps[0], self.prefix)  # Category
-    #             category += '    '
-    #         else:
-    #             self.dic_categ[nwps[0]] += 1         # increment
-    #             category = '    '
-    #     else:                                        # no subfolder
-    #         if self.first:
-    #             category = '* $pdf \n    +++ {0} \n'.format(self.prefix)
-    #             self.first = False
-    #         else:
-    #             category = ''
 
     def list_corr(self, arg):
         '''
