dfa705670f4757b6d0f7a4e9cd95e2eff8b8af61
aspilio <lio.chiron@google.com>
2019-02-04 23:49:16 +0100

    improving move_walls_save_positions

---
+++ js/threejs_exp/moving_walls/move_walls_save_positions/static/js/object_museum.js
@@ -0,0 +1,116 @@
+disp = 5
+function make_uniform_ground(){
+    //alert("in make_board")
+    var size_square = 2000;
+    var geometry = new THREE.CubeGeometry( size_square, size_square, 2 );
+    var square_color = 0xffffff
+    var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: square_color } ) );
+    object.material.ambient = object.material.color;
+    object.position.x = 0
+    object.position.y = 0
+    object.position.z = 0
+    object.castShadow = true;
+    object.receiveShadow = true;
+    scene.add( object );
+} // end function
+function make_ground_chess(){
+    //alert("in make_board")
+    var size_square = 150;
+    var geometry = new THREE.CubeGeometry( size_square, size_square, 5 );
+    var square_color
+    for ( var i = 0; i < 64; i ++ ) {
+        if ((i+Math.floor(i/8))%2==0){square_color = 0x000000}
+        else{square_color = 0xffffff}
+        var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: square_color } ) );
+        object.material.ambient = object.material.color;
+        object.position.x = (i%8-4) * size_square
+        object.position.y = (Math.floor(i/8)-4) * size_square
+        object.position.z = 0
+        object.castShadow = true;
+        object.receiveShadow = true;
+        scene.add( object );
+        // objects.push( object );
+    }
+} // end function
+
+function make_wall(name,p){
+    //alert("in make_wall")
+    //alert( "Stringify " + JSON.stringify(obj))
+    //alert(JSON.stringify(dicpos))
+    //console.log('Heeeeeeeeeeeeeeeeeeeeeeeerrrrrrrrrrrrrrrrrrrreeee')
+    wall_color = 0x800000
+    var wall_length = 150;
+    var wall_width = 5;
+    var wall_height = 300;
+    var geometry = new THREE.CubeGeometry( wall_width, wall_length, wall_height );
+    var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: wall_color } ) );
+    object.material.ambient = object.material.color;
+    object.position.x = p.x //obj.x //dicpos.x
+    object.position.y = p.y //obj.y //dicpos.y
+    object.position.z = wall_height/2
+    object.castShadow = true;
+    object.receiveShadow = true;
+    object.name = name;
+
+    scene.add( object );
+    objects.push(object)
+
+    return object
+
+} // end function
+
+function make_small_seats(){
+    //alert("in make_small_seats")
+    var size_square = 150;
+    var size_pawns = 50;
+    var geometry = new THREE.CubeGeometry( size_pawns, size_pawns, size_pawns );
+    var square_color;
+    for ( var i = 0; i < 16; i ++ ) {
+        if (Math.floor(i/8)%2==0){
+            square_color = 0x800000
+            var posy = 1
+           }
+        else{
+            square_color = 0xf0f0f5
+            var posy = 6
+           }
+        var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: square_color } ) );
+        object.material.ambient = object.material.color;
+        object.position.x = disp*Math.random() * size_square
+        object.position.y = disp*Math.random() * size_square
+        object.position.z = size_pawns/2
+        object.castShadow = true;
+        object.receiveShadow = true;
+        //object.name = "small_seat_"+i
+        scene.add( object );
+        //objects.push( {"small_seat_"+i : object} );
+    } // end for
+} // end function
+
+function make_seat(){
+    //alert("in make_pawns")
+    var size_square = 150;
+    var size_seat = 100;
+    var geometry = new THREE.CylinderGeometry( 50, 25, size_seat);
+    var square_color;
+    for ( var i = 0; i < 4; i ++ ) {
+        if (Math.floor(i/2)%2==0){
+            square_color = 0x800000
+            var posy = 1
+           }
+        else{
+            square_color = 0xf0f0f5
+            var posy = 6
+           }
+        var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: square_color } ) );
+        object.material.ambient = object.material.color;
+        object.position.x = disp*Math.random() * size_square
+        object.position.y = disp*Math.random() * size_square
+        object.position.z = size_seat/2
+        object.rotation.x = -Math.PI/2
+        object.castShadow = true;
+        object.receiveShadow = true;
+        scene.add( object );
+        //objects.push( object );
+    } // end for
+} // end function
